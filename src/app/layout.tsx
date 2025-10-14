73fdd5da574c6501c4a7eba02132f8178a6c82c4
aniketprajapati.bizline@linics.co <aniketprajapati.bizline@linics.co>
2025-10-14 12:18:05 +0530

    Google adds Script added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,11 @@
-import type {Metadata} from 'next';
-import './globals.css';
-import { Toaster } from "@/components/ui/toaster"
+import type { Metadata } from "next";
+import "./globals.css";
+import { Toaster } from "@/components/ui/toaster";
 
 export const metadata: Metadata = {
-  title: 'દિવાળી શુભેચ્છાઓ',
-  description: 'તમારા મિત્રો અને પરિવાર માટે સુંદર, વ્યક્તિગત દિવાળી શુભેચ્છાઓ બનાવો અને શેર કરો.',
+  title: "દિવાળી શુભેચ્છાઓ",
+  description:
+    "તમારા મિત્રો અને પરિવાર માટે સુંદર, વ્યક્તિગત દિવાળી શુભેચ્છાઓ બનાવો અને શેર કરો.",
 };
 
 export default function RootLayout({
@@ -16,8 +17,20 @@ export default function RootLayout({
     <html lang="gu" className="dark">
       <head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
-        <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+Gujarati:wght@400;700;900&family=Playfair+Display:wght@700;900&display=swap" rel="stylesheet" />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="true"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Noto+Sans+Gujarati:wght@400;700;900&family=Playfair+Display:wght@700;900&display=swap"
+          rel="stylesheet"
+        />
+        <script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9496801021839924"
+          crossorigin="anonymous"
+        ></script>
       </head>
       <body className="font-body antialiased">
         {children}
